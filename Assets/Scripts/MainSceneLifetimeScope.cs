92129d942c4ab3ab0feedd1997b4b2ac92ef0328
k-IshiguroHaruya <h-ishiguro@kakusin-g.co.jp>
2021-08-03 00:00:45 +0900

    UniRx・VContainerインポート

---
+++ Assets/Scripts/MainSceneLifetimeScope.cs
@@ -0,0 +1,14 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace MagicCube
+{
+    public class MainSceneLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            
+        }
+    }
+
+}
